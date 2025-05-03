01323fc4270faa13417ebfae64f20f280d971037
Quentin RA <quentin.ramsamy.cf@gmail.com>
2025-05-03 16:04:24 +0200

    [feat] release version 0.8.2

--- pkg/providers/inthewild.go
+++ pkg/providers/inthewild.go
@@ -20,10 +20,17 @@ func ParseInTheWild(jsonFilePath string) ([]*providertypes.InTheWild, error) {
 	}
 
 	var final []*providertypes.InTheWild
+	addedURLs := make(map[string]struct{})
 	for _, candidate := range data {
 		candidate.CveID = utils.CleanCVE(candidate.CveID)
 		candidate.ReportURL, candidate.Score = InspectAggregatorURL(candidate.ReportURL, candidate.CveID, true)
 		if candidate.ReportURL != "" {
+			if _, exists := addedURLs[candidate.ReportURL]; exists {
+				continue
+			}
+			// Only add URLs once
+			addedURLs[candidate.ReportURL] = struct{}{}
+			// Add to the list
 			final = append(final, candidate)
 		}
 	}
